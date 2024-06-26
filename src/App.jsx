34ef024238499b88be31aa2bf4ea523f7cb73407
Syed Mohammad Easin <syedeasin11000@gmail.com>
2024-06-26 09:28:54 +0600

    Lifting up the state from Board component

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,37 @@
 import { useState } from "react";
 
-function Square() {
-  const [value, setValue] = useState(null);
-
-  function handleClick() {
-    setValue("X");
-  }
-
+function Square({ value, onSquareClick }) {
   return (
     <button
       className="bg-white border border-gray-500 h-12 w-12 m-1 text-lg font-bold"
-      onClick={handleClick}
+      onClick={onSquareClick}
     >
       {value}
     </button>
   );
 }
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  function handleClick(){
+    setSquares('X');
+  }
+
   return (
     <>
       <div className="flex">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} onSquareClick={handleClick} />
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
       </div>
       <div className="flex">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
       </div>
       <div className="flex">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
       </div>
     </>
   );
